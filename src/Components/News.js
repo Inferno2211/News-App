69ddd45bb07d632affd01f92a781bad6fb8c3ed9
Inferno2211 <abirjerry2211@gmail.com>
2024-07-14 23:50:49 +0530

    Use google api

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,84 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import NewsCard from './NewsCard';
 
-const apiKey =  process.env.REACT_APP_NEWS_API_KEY;
+const apiKey = process.env.REACT_APP_NEWS_API_KEY;
 
 const News = () => {
-    const [isTop, setIsTop] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [selectedCountry, setSelectedCountry] = useState('');
-    const [selectedSort, setSelectedSort] = useState('');
-    const [selectedLanguage, setSelectedLanguage] = useState('');
+    const [selectedOption, setSelectedOption] = useState('latest'); // Default option
     const [newsData, setNewsData] = useState([]);
-    const [query, setQuery] = useState('technology');
-    const [searchQuery, setSearchQuery] = useState('');
     const [res, setRes] = useState('');
 
-    const switchMode = () => {
-        setIsTop((isTop) => !isTop);
+    const handleOptionChange = (e) => {
+        setSelectedOption(e.target.value);
     };
 
-    const handleCategoryChange = (e) => {
-        setSelectedCategory(e.target.value);
-    };
-
-    const handleCountryChange = (e) => {
-        setSelectedCountry(e.target.value);
-    };
-
-    const handleSortChange = (e) => {
-        setSelectedSort(e.target.value);
-    };
-
-    const handleLanguageChange = (e) => {
-        setSelectedLanguage(e.target.value);
-    };
-
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            setQuery(searchQuery);
-        }
-    };
-
-    const handleRes = (e) =>{
-        setRes(e);
-    }
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                let url = '';
-                if (isTop) {
-                    url = `https://newsapi.org/v2/top-headlines?`;
-                    if(query !=="")
-                        url += `q=${query}&`;
-                    if(selectedCountry !=="")
-                        url += `country=${selectedCountry}&`;
-                    if(selectedCategory !=="")
-                        url += `category=${selectedCategory}&`;
-                    url += `apiKey=${apiKey}`;
+                const url = `https://google-news13.p.rapidapi.com/${selectedOption}?lr=en-US`;
+                const headers = {
+                    'x-rapidapi-host': 'google-news13.p.rapidapi.com',
+                    'x-rapidapi-key': apiKey,
+                };
+                const response = await fetch(url, { headers });
+                const parsedData = await response.json();
+
+                if (parsedData.status !== 'success') {
+                    setRes('Failed to fetch news'); // Handle API errors
                 } else {
-                    url = `https://newsapi.org/v2/everything?`;
-                    if(query !=="")
-                        url += `q=${query}&`;
-                    if(selectedSort !=="")
-                        url += `sortBy=${selectedSort}&`;
-                    if(selectedLanguage !=="")
-                        url += `language=${selectedLanguage}&`;
-                    url += `apiKey=${apiKey}`;
-                    }
-                let data = await fetch(url);
-                let parsedData = await data.json();
-                if(parsedData.status!=='ok'){
-                    handleRes(parsedData.message);
-                }
-                else if(parsedData.totalResults === 0){
-                    handleRes("No results for selected parameters");
-                }
-                else{
-                    handleRes("");
+                    setNewsData(parsedData.items);
+                    setRes(''); // Clear previous error messages
                 }
 
-                setNewsData(parsedData.articles);
                 console.log(url);
                 console.log(parsedData);
             } catch (error) {
@@ -87,73 +38,25 @@ const News = () => {
         };
 
         fetchNews();
-    }, [isTop, selectedCategory, selectedCountry, selectedSort, selectedLanguage, query]);
+    }, [selectedOption]);
 
     return (
         <div className='news-page'>
             <div className="sidebar">
-                {isTop 
-                    ? <div  className="top-headlines">
-                        <h3>Filter by Category</h3>
-                        <select onChange={handleCategoryChange} value={selectedCategory}>
-                            <option value="">Any</option>
-                            <option value="general">General</option>
-                            <option value="business">Business</option>
-                            <option value="entertainment">Entertainment</option>
-                            <option value="health">Health</option>
-                            <option value="science">Science</option>
-                            <option value="sports">Sports</option>
-                            <option value="technology">Technology</option>
-                        </select>
-
-                        <h3>Filter by Country</h3>
-                        <select onChange={handleCountryChange} value={selectedCountry}>
-                            <option value="">Any</option>
-                            <option value="in">India</option>
-                            <option value="us">United States</option>
-                            <option value="gb">United Kingdom</option>
-                            <option value="au">Australia</option>
-                            <option value="ca">Canada</option>
-                        </select>
-                    </div>
-
-                    : <div className='everything'>
-                        <h3>Sort by:</h3>
-                        <select onChange={handleSortChange} value={selectedSort}>
-                            <option value="relevancy">Relevancy</option>
-                            <option value="popularity">Popularity</option>
-                            <option value="publishedAt">Published date</option>
-                        </select>
-
-                        <h3>Filter by Language</h3>
-                        <select onChange={handleLanguageChange} value={selectedLanguage}>
-                            <option value="">Any</option>
-                            <option value="zh">Chinese</option>
-                            <option value="en">English</option>
-                            <option value="fr">French</option>
-                            <option value="de">German</option>
-                            <option value="it">Italian</option>
-                            <option value="nl">Dutch</option>
-                            <option value="ru">Russian</option>
-                            <option value="es">Spanish</option>
-                        </select>
-                    </div>
-                }
+                <h3>Select Category:</h3>
+                <select onChange={handleOptionChange} value={selectedOption}>
+                    <option value="latest">Latest</option>
+                    <option value="entertainment">Entertainment</option>
+                    <option value="world">World</option>
+                    <option value="business">Business</option>
+                    <option value="health">Health</option>
+                    <option value="sport">Sport</option>
+                    <option value="science">Science</option>
+                    <option value="technology">Technology</option>
+                </select>
             </div>
 
             <div className='main-container'>
-                <div className='top-container'>
-                    <button onClick={switchMode}>
-                        {isTop ? 'Search all' : 'Latest Headlines'}
-                    </button>
-
-                    <input 
-                        type='text' 
-                        placeholder='Search' 
-                        onChange={e => setSearchQuery(e.target.value)} 
-                        onKeyPress={handleKeyPress}
-                    />
-                </div>
                 <p>{res}</p>
                 <div className='news-list'>
                     {newsData && newsData.map((article, index) => (
--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const NewsCard = ({ article }) => {
+    // Convert timestamp to Date object
+    const date = new Date(parseInt(article.timestamp));
+
+    // Format date and time
+    const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    const formattedTime = `${date.getHours()}:${(date.getMinutes() < 10 ? '0' : '') + date.getMinutes()}`;
+
     return (
         <div className="news-card">
             <div className="title-date">
                 <h3>{article.title}</h3>
-                <p className='date'>{new Date(article.publishedAt).toLocaleString()}</p>
+                <p className='date'>{formattedTime} / {formattedDate}</p>
             </div>
-            {article.urlToImage && <img src={article.urlToImage} alt={article.title} />}
+            {article.images && article.images.length > 0 && (
+                <img src={article.images[0].thumbnail} alt={article.title} />
+            )}
             <div className="content-link">
-                <p>{article.description}</p>
-                <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                <p>{article.snippet}</p>
+                <a href={article.newsUrl} target="_blank" rel="noopener noreferrer">Read more</a>
             </div>
         </div>
     );
